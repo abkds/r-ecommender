50acdcfb5c81f368fdd074c05fc9b79c2ea8bbac
Abhishek K Das <abhishek.das.contact@gmail.com>
2019-04-27 12:28:42 -0500

    Add: Interactions class in utility

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 
 def item_to_index(things):
@@ -13,3 +14,20 @@ def index_to_item(index):
     for item, idx in index.items():
         things[idx] = item
     return things
+
+
+class Interaction():
+    "Deal with basic manipulations of Interaction object"
+    def __init__(self, interactions, num_users, num_items):
+        self.interactions = interactions
+        self.num_users = num_users
+        self.num_items = num_items
+        self.interactions_dict = None
+
+    def get_interaction_dict(self):
+        "Returns the interactions in the form of dictionary"
+        if self.interactions_dict is None:
+            self.interactions_dict = defaultdict(lambda: set())
+            for user, item, _ in self.interactions:
+                self.interactions_dict[user].add(item)
+        return self.interactions_dict
