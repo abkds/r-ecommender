9546dc6a3eeeafea191778c9490ad48ab874238e
Abhishek K Das <abhishek.das.contact@gmail.com>
2019-04-28 13:30:01 -0500

    refactor: move auc to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tqdm
 from collections import defaultdict
 
 
@@ -18,6 +19,7 @@ def index_to_item(index):
 
 class Interaction():
     "Deal with basic manipulations of Interaction object"
+
     def __init__(self, interactions, num_users, num_items):
         self.interactions = interactions
         self.num_users = num_users
@@ -27,7 +29,34 @@ class Interaction():
     def get_interaction_dict(self):
         "Returns the interactions in the form of dictionary"
         if self.interactions_dict is None:
-            self.interactions_dict = defaultdict(lambda: defaultdict(lambda: 0))
+            self.interactions_dict = defaultdict(
+                lambda: defaultdict(lambda: 0))
             for user, item, count in self.interactions:
                 self.interactions_dict[user][item] = int(count)
         return self.interactions_dict
+
+
+def auc(test_set, user_factors, subreddit_factors, subreddits, users):
+    """
+    Returns the auc score on a test data set
+    """
+    num_users = len(test_set)
+    total = 0
+
+    # treat the signal as 1 as per the implicit bpr paper
+    for subreddit, user, signal in tqdm.tqdm_notebook(test_set):
+        u = users_index[user]
+        i = subreddits_index[subreddit]
+
+        x_ui = user_factors[u].dot(subreddit_factors[i])
+
+        js = []
+
+        for j in range(0, num_subreddits):
+            if j != i and j not in E_u[u]:
+                js.append(j)
+
+        total += np.sum(np.heaviside(x_ui - \
+                        user_factors[u].dot(subreddit_factors[js].T), 0)) / len(js)
+
+    return total / num_users
